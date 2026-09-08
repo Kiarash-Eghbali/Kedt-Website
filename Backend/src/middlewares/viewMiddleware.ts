14f8c5da7d312441e15d8baa38e5711aa39642ea
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-08 13:58:00 +0330

    Complete viewMiddleware.ts

--- Backend/src/middlewares/viewMiddleware.ts
+++ Backend/src/middlewares/viewMiddleware.ts
@@ -1,15 +1,16 @@
 import { type Request, type Response, type NextFunction } from "express";
-import ResponseInterface from "../types/ResponseInterface";
+
 
 const viewMiddleware = function (req: Request, res: Response, next: NextFunction) {
-    const response: ResponseInterface = {
-        result: "success",
-        code: "VIEW_AUTH_SUCCESSED",
-        statusCode: 200,
-    }
-     
-    res.status(200).json(response);
-    next();
+    const cookie = req.cookies;
+    if (!cookie.viewed) {
+        req.body.newViewer = false;
+        next();
+    } else {
+        req.body.newViewer = true;
+        next();
+    };
+
 };
 
 export default viewMiddleware;
