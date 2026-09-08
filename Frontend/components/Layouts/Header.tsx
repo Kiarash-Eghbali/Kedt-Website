6c9ccff6928164fa108c8721467fad5c352e24ce
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-08 17:35:51 +0330

    add Header and nav to Header.tsx

--- Frontend/components/Layouts/Header.tsx
+++ Frontend/components/Layouts/Header.tsx
@@ -1,11 +1,29 @@
-function Header() {
-    return (
-        <>
-            <header>
+import Link from "next/link";
+import Image from "next/image";
+import pic from "../../public/nano-banana-3bbbe006478acfc767060b100a5c70ab-1_-_Copy-removebg-preview.webp";
+import Navbar from "./Headers/navbar";
+import ThemeButton from "./Headers/themeButton";
 
-            </header>
-        </>
-    )
-};
+function Header() {
+	return (
+		<>
+			<header className="w-full fixed top-0 border-b border-gray-200 px-8 py-3 flex items-center justify-between ">
+				<div className="flex items-center justify-center">
+					<Link
+						href={"/"}
+						className="w-15 h-15  md:w-20 md:h-20"
+					>
+						<Image
+							src={pic}
+							alt={"Kedt-Team-pic"}
+						/>
+					</Link>
+				</div>
+                <Navbar />
+                <ThemeButton />
+			</header>
+		</>
+	);
+}
 
 export default Header;
