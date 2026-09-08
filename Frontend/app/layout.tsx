cc1e67a5fee711c891950fca87dde1948509cfc6
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-09 01:47:15 +0330

    Add new font to layout.tsx

--- Frontend/app/layout.tsx
+++ Frontend/app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
-import { Vazirmatn } from "next/font/google";
+import { Vazirmatn, Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Layouts/Header";
 import Footer from "@/components/Layouts/Footer";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 
 const vazirMatn = Vazirmatn({ subsets: ["arabic"] });
+const poppins = Poppins({ subsets: ["latin"], variable: "--font-poppins", weight: ["500"] });
 
 export const metadata: Metadata = { title: "KEDT | تیم توسعه دهنده کیارش اقبالی", description: "KEDT website created with NextJs , Typescript , Tailwindcss , ExpressJs , NodeJs" };
 
@@ -14,7 +15,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
 		<html
 			lang="fa"
 			dir="rtl"
-			className={`${vazirMatn.className}  h-full antialiased`}
+			className={`${vazirMatn.className} h-full antialiased`}
 		>
 			<ThemeProvider>
 				<Header />
