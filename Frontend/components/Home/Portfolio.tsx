a83f4e3f403b0be9ca6b851b0fcad9d0d5f6dd0b
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-10 01:28:06 +0330

    Add eager loading to image Portfolio

--- Frontend/components/Home/Portfolio.tsx
+++ Frontend/components/Home/Portfolio.tsx
@@ -14,6 +14,7 @@ function Portfolio({ image, title, body, link, className, languages, classLang, 
 						src={image}
 						alt={title}
 						className={`w-65 h-40 rounded-xl mx-auto mb-5 `}
+						loading="eager"
 					/>
 				</div>
 
