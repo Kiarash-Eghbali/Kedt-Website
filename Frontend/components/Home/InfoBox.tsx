747331518d7a2a9b0eecc7068461772287948621
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-09 14:06:20 +0330

    Flexible InfoBox and InfoButton in home page

--- Frontend/components/Home/InfoBox.tsx
+++ Frontend/components/Home/InfoBox.tsx
@@ -8,15 +8,15 @@ function InfoBox() {
     const { isDark } = useThemeContext();
 	return (
 		<>
-			<div className="flex items-center justify-center flex-col">
+			<div className={`flex items-center justify-center flex-col w-[90%] md:w-[80%] mx-auto border-b pb-10 ${isDark ? "border-[#252525]" : "border-gray-400"}`}>
 				<div className="flex items-center justify-center flex-col gap-2">
                     <div className={`border ${isDark ? "bg-[#202020] border-blue-500/30 " : "bg-gray-500/5 border-blue-500"} mt-10 p-2 rounded-xl   flex items-center justify-center gap-1`}>
                         <div className="w-2 h-2 bg-blue-500 rounded-full animate-pulse"></div>
                         <h2 className={`text-[12px] font-bold ${poppins.className}`}>Full Stack Developer</h2>
                     </div>
-					<h1 className={` text-4xl pt-7 ${isDark ? "text-gray-300" : "" } font-black ${poppins.className}`}>Kiarash Eghbali</h1>
-                    <p className={`font-bold ${isDark ? "text-blue-400" : "text-gray-600"}`}>طراح و توسعه دهنده وبسایت های مدرن</p>
-                    <p className={`text-[11px] mt-5 m-1 ${isDark ? "text-blue-100" : "text-gray-500"} text-center`}>من کیارش اقبالی، توسعه‌دهنده فول‌استک ۱۶ ساله از ایرانم. با React، Next.js، TypeScript، Express و Laravel کار می‌کنم و عاشق ساختن چیزهای تمیز و مدرنم.</p>
+					<h1 className={` text-4xl md:text-6xl pt-7 ${isDark ? "text-gray-300" : "" } font-black ${poppins.className}`}>Kiarash Eghbali</h1>
+                    <p className={`font-bold md:text-2xl ${isDark ? "text-blue-400" : "text-gray-600"}`}>طراح و توسعه دهنده وبسایت های مدرن</p>
+                    <p className={`text-[11px] md:text-base mt-5 m-1 w-[70%] ${isDark ? "text-blue-100" : "text-gray-500"} text-center`}>من کیارش اقبالی، توسعه‌دهنده فول‌استک ۱۶ ساله از ایرانم. با React، Next.js، TypeScript، Express و Laravel کار می‌کنم و عاشق ساختن چیزهای تمیز و مدرنم.</p>
                     <InfoButton />
 				</div>
 			</div>
--- Frontend/components/Home/InfoButton.tsx
+++ Frontend/components/Home/InfoButton.tsx
@@ -6,15 +6,15 @@ function InfoButton() {
     const { isDark } = useThemeContext();
     return (
         <>
-            <div className={`flex items-center justify-around w-full mt-4`}>
+            <div className={`flex items-center justify-around w-[80%] md:w-[60%] mt-4`}>
                 <Link href={"/contact-me"} className={isDark
-                     ? "bg-[#252525] p-2 px-5 rounded-md text-sm  hover:bg-blue-600 transition-all duration-150" 
-                     : "bg-blue-500 p-2 px-5 rounded-md text-sm hover:bg-blue-600 transition-all duration-150 text-white"
+                     ? "bg-[#252525] p-2 px-5 rounded-md text-sm md:text-lg hover:bg-blue-600 transition-all duration-150" 
+                     : "bg-blue-500 p-2 px-5 rounded-md text-sm md:text-lg hover:bg-blue-600 transition-all duration-150 text-white"
                      
                 }>تماس با من</Link>
                 <Link href={"/"} className={isDark 
-                    ? "border border-[#252525] p-2 px-5 rounded-md text-sm hover:bg-[#252525] transition-all duration-150" 
-                    : "border border-gray-600 p-2 px-5 rounded-md text-sm hover:bg-gray-100 text-gray-600 transition-all duration-150"
+                    ? "border border-[#252525] p-2 px-5 rounded-md text-sm md:text-lg hover:bg-[#252525] transition-all duration-150" 
+                    : "border border-gray-600 p-2 px-5 rounded-md text-sm md:text-lg hover:bg-gray-100 text-gray-600 transition-all duration-150"
                 
                 }>نمونه کار ها</Link>
             </div>
