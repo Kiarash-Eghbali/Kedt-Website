b355bc905600a5a6cf0b3d0a6ee8aab488719879
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-09 13:37:59 +0330

    Add width to all section in homepage

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -3,7 +3,7 @@ import InfoBox from "@/components/Home/InfoBox";
 function homePage() {
 	return (
 		<>
-			<section className="mt-30">
+			<section className="mt-30 md:w-[80%] mx-auto">
 				<InfoBox />
 			</section>
 		</>
