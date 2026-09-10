c93d9dca8dc8db64845e53ad002461bffcde8548
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-11 00:13:45 +0330

    Add footer component and flexible footer

--- Frontend/components/Layouts/Footer.tsx
+++ Frontend/components/Layouts/Footer.tsx
@@ -1,7 +1,23 @@
+"use client";
+import { useThemeContext } from "@/contexts/ThemeContext";
+import InfoFooter from "./Footers/InfoFooter";
+import LinkFooter from "./Footers/LinkFooter";
+import SocialFooter from "./Footers/SocialFooter";
+
+
 function Footer() {
+	const { isDark } = useThemeContext();
+
 	return (
 		<>
-			<footer></footer>
+			<footer className={`pt-5 mt-30 border-t w-full grid grid-cols-1 md:grid-cols-3  items-center justify-center  ${isDark ? "border-[#252525]" : "border-gray-400"}`}>
+				<InfoFooter />
+				<LinkFooter />
+				<SocialFooter />
+				<div className={`py-3 md:col-span-3`}>
+					<h1 className={`text-xs md:text-base ${isDark ? "text-[#d1d1d1] font-bold" : "text-gray-700 font-bold"} pt-3 text-center`}> طراحی شده توسط کیارش اقبالی {new Date().getFullYear()} ©</h1>
+				</div>
+			</footer>
 		</>
 	);
 }
