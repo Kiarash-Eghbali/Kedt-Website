b9dce6303cf81d48ed98db076d0b9e30bcf6d737
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-10 13:57:35 +0330

    Add poppins font to Skill

--- Frontend/components/Home/Skill.tsx
+++ Frontend/components/Home/Skill.tsx
@@ -2,6 +2,8 @@
 import { SiJavascript, SiTypescript, SiReact, SiNextdotjs, SiTailwindcss, SiNodedotjs, SiExpress, SiPhp, SiLaravel, SiMongodb, SiWordpress } from "react-icons/si";
 import SkillInterface from "@/types/SkillInterface";
 import { useThemeContext } from "@/contexts/ThemeContext";
+import { Poppins } from "next/font/google";
+const poppins = Poppins({ subsets: ["latin"], variable: "--font-poppins", weight: ["500"] });
 
 function Skill({ icon: Icon, name, className, classIcon, classLabel }: SkillInterface) {
     const { isDark } = useThemeContext();
@@ -11,7 +13,7 @@ function Skill({ icon: Icon, name, className, classIcon, classLabel }: SkillInte
 				<div className={`  ${className ? className : ` group-hover:translate-y-[-10px] transition-all duration-150 ${isDark ? "bg-[#252525]" : "group-hover:bg-white"} w-14 h-14 md:w-17 md:h-17 lg:w-20 lg:h-20 flex items-center justify-center rounded-lg `} `}>
 					<Icon className={` ${classIcon ? classIcon : ""} ${isDark ? "text-[whitesmoke]" : "text-[#555555]"} text-3xl md:text-4xl lg:text-5xl transition-all duration-150`} />
 				</div>
-				<h1 className={`text-xl text-transparent ${isDark ? "group-hover:text-white" : "group-hover:text-[#252525]"} transition-all duration-150 ${classLabel ? classLabel : ""} select-none`}>{name}</h1>
+				<h1 className={`text-xl ${poppins.className} text-transparent ${isDark ? "group-hover:text-white" : "group-hover:text-[#252525]"} transition-all duration-150 ${classLabel ? classLabel : ""} select-none`}>{name}</h1>
 			</div>
 		</>
 	);
