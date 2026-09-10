7894e0343a85df6324b2d23f4c85bb8f0c2ad7fd
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-10 14:32:10 +0330

    Fix Type Error in ThemeContext.tsx

--- Frontend/contexts/ThemeContext.tsx
+++ Frontend/contexts/ThemeContext.tsx
@@ -15,4 +15,10 @@ export function ThemeProvider({ children }: { children: ReactNode }) {
 	);
 }
 
-export const useThemeContext = () => useContext(ThemeContext);
+export function useThemeContext(): ThemeContextType {
+    const context = useContext(ThemeContext);
+    if (!context) {
+        throw new Error("useThemeContext must be used within ThemeProvider");
+    }
+    return context;
+}
