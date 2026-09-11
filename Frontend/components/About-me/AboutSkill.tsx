32ea23be8b719855cf6f3cc60fb5cd8c94d53dc8
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-11 17:31:08 +0330

    Create About box and About skill component

---
+++ Frontend/components/About-me/AboutSkill.tsx
@@ -0,0 +1,13 @@
+import SkillInfoBox from "../Home/SkillSection/SkillInfoBox";
+
+function AboutSkill() {
+    return (
+        <>
+            <section>
+                <SkillInfoBox />
+            </section>
+        </>
+    )
+};
+
+export default AboutSkill;
