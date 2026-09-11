129b594946a8437c04c98dd1680a341bcc8db784
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-11 22:37:36 +0330

    Timeline component flexied

--- Frontend/components/About-me/AboutTimeline/Timeline.tsx
+++ Frontend/components/About-me/AboutTimeline/Timeline.tsx
@@ -6,7 +6,7 @@ function Timeline({ year, title, content, className, classYear, classTitle, clas
     const { isDark } = useThemeContext();
     return (
         <>
-            <div className={` ${className ? className : ""} flex items-center justify-center gap-3 px-2 py-3 rounded-md ${isDark ? "bg-[#252525]" : ""}`}>
+            <div className={` ${className ? className : ""}w-full md:w-[80%] flex items-center justify-start gap-3 px-2 py-3 rounded-md ${isDark ? "bg-[#252525]" : ""}`}>
                 <div className={`px-2 py-3 pt-4 md:px-4 md:py-5 md:pt-6 flex ${isDark ? "bg-[#151515] rounded-md" : ""} items-center justify-center`}>
                     <h1 className={`${classYear ? classYear : ""} md:text-xl font-bold ${isDark ? "text-gray-100" : ""}`}>{year}</h1>
                 </div>
