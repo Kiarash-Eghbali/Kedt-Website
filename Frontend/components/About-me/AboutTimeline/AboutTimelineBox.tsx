9f452fee1f8da021a90fda7ccaea1eaf0d1ab821
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-11 21:28:36 +0330

    Add title to AboutTimelineBox

--- Frontend/components/About-me/AboutTimeline/AboutTimelineBox.tsx
+++ Frontend/components/About-me/AboutTimeline/AboutTimelineBox.tsx
@@ -1,9 +1,13 @@
+"use client";
+import { useThemeContext } from "@/contexts/ThemeContext";
 import AboutTimeline from "./AboutTimeline";
 
 function AboutTimelineBox() {
+    const { isDark } = useThemeContext();
     return (
         <>
             <section className={`mt-10 flex items-center justify-center flex-col`}>
+                <h1 className={`text-xl text-center ${isDark ? "text-[#888888] font-bold" : "text-gray-600 font-bold"}`}>مسیر یادگیری</h1>
                 <AboutTimeline />
             </section>
         </>
