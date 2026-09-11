2fb8ce6254100f4892f7caad116c829d9472f856
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-12 01:29:39 +0330

    feat(timeline): add light theme to timeline

--- Frontend/components/About-me/AboutTimeline/Timeline.tsx
+++ Frontend/components/About-me/AboutTimeline/Timeline.tsx
@@ -15,13 +15,13 @@ function Timeline({ year, title, content, className, classYear, classTitle, clas
 
     return (
         <>
-            <div className={`w-[95%] mx-auto flex md:w-[80%] items-center px-1 py-3 rounded-md flex-col ${isDark ? "bg-[#252525]" : ""}`}>
+            <div className={`w-[95%] mx-auto flex md:w-[80%] items-center px-1 py-3 rounded-md flex-col ${isDark ? "bg-[#252525]" : "bg-[#eeeeee] border border-[#d1d1d1]"}`}>
                 <div className={`${className ? className : ""} self-start w-full flex items-center gap-3 px-1`}>
-                    <div className={`px-2 py-3 pt-4 md:px-4 md:py-5 md:pt-6 flex ${isDark ? "bg-[#151515] rounded-md" : ""} items-center justify-center`}>
-                        <h1 className={`${classYear ? classYear : ""} md:text-xl font-bold ${isDark ? "text-gray-100" : ""}`}>{year}</h1>
+                    <div className={`px-2 py-3 pt-4 md:px-4 md:py-5 md:pt-6 flex ${isDark ? "bg-[#151515] rounded-md" : "bg-white rounded-md"} items-center justify-center`}>
+                        <h1 className={`${classYear ? classYear : ""} md:text-xl font-bold ${isDark ? "text-gray-100" : "text-[#252525]"}`}>{year}</h1>
                     </div>
                     <div className={`grid grid-cols-4 md:grid-cols-1 w-full`}>
-                        <h1 className={`${classTitle ? classTitle : ""} col-span-3 text-xs md:text-xl ${isDark ? "text-gray-300" : ""}`}>{title}</h1>
+                        <h1 className={`${classTitle ? classTitle : ""} col-span-3 text-xs md:text-xl ${isDark ? "text-gray-300" : "text-[#111111]"}`}>{title}</h1>
                         <span className="text-left pl-2 md:hidden">
                             <FontAwesomeIcon
                                 icon={faArrowRight}
@@ -29,7 +29,7 @@ function Timeline({ year, title, content, className, classYear, classTitle, clas
                                 className={`text-lg font-bold cursor-pointer ${isOpen ? "rotate-90" : ""} transition-all duration-150`}
                             />
                         </span>
-                        <h2 className={`${classContent ? classContent : ""} hidden md:inline-block md:h-20 lg:h-15 text-sm pt-2 ${isDark ? "text-[#9e9e9e]" : ""}`}>{content}</h2>
+                        <h2 className={`${classContent ? classContent : ""} hidden md:inline-block md:h-20 lg:h-15 text-sm pt-2 ${isDark ? "text-[#9e9e9e]" : "text-gray-700"}`}>{content}</h2>
                     </div>
                 </div>
                 
