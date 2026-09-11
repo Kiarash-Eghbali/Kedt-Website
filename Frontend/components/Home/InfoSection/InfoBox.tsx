a782fdf15eec37afd7a6f937f1e09ae14b3855ee
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-11 15:33:24 +0330

    Optimize font in InfoBox component

--- Frontend/components/Home/InfoSection/InfoBox.tsx
+++ Frontend/components/Home/InfoSection/InfoBox.tsx
@@ -1,6 +1,4 @@
 "use client";
-import { Poppins } from "next/font/google";
-const poppins = Poppins({ subsets: ["latin"], variable: "--font-poppins", weight: ["500"] });
 import { useThemeContext } from "@/contexts/ThemeContext";
 import InfoButton from "./InfoButton";
 
@@ -12,9 +10,9 @@ function InfoBox() {
 				<div className="flex items-center justify-center flex-col gap-2">
 					<div className={`border ${isDark ? "bg-[#202020] border-blue-500/30 " : "bg-gray-500/5 border-blue-500"}  p-2 rounded-xl   flex items-center justify-center gap-1`}>
 						<div className="w-2 h-2 bg-blue-500 rounded-full animate-pulse"></div>
-						<h2 className={`text-[12px] font-bold ${poppins.className}`}>Full Stack Developer</h2>
+						<h2 className={`text-[12px]  font-[family-name:var(--font-poppins)]`}>Full Stack Developer</h2>
 					</div>
-					<h1 className={` text-4xl md:text-6xl pt-7 ${isDark ? "text-gray-300" : ""} font-black ${poppins.className}`}>Kiarash Eghbali</h1>
+					<h1 className={` text-4xl md:text-6xl pt-7 ${isDark ? "text-gray-300" : ""}  font-[family-name:var(--font-poppins)]`}>Kiarash Eghbali</h1>
 					<p className={`font-bold md:text-2xl ${isDark ? "text-blue-400" : "text-gray-600"}`}>طراح و توسعه دهنده وبسایت های مدرن</p>
 					<p className={`text-[11px] md:text-base mt-5 m-1 w-[70%] ${isDark ? "text-blue-300" : "text-gray-700"} text-center`}>
 						من کیارش اقبالی، توسعه‌دهنده فول‌استک ۱۶ ساله از ایرانم. با React، Next.js، TypeScript، Express و Laravel کار می‌کنم و عاشق ساختن چیزهای تمیز و مدرنم.
