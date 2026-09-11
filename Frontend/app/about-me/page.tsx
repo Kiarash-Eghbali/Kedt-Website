a60bc88da6abd83a837a11f4843ae532432c1d6f
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-11 17:31:29 +0330

    Add About-me components to about page

--- Frontend/app/about-me/page.tsx
+++ Frontend/app/about-me/page.tsx
@@ -1,11 +1,18 @@
+import AboutBox from "@/components/About-me/AboutBox";
+import AboutSkill from "@/components/About-me/AboutSkill";
+
 function aboutMe() {
-    return (
-        <>
-            <main className="md:w-[80%] flex items-center justify-center flex-col gap-30 mx-auto">
-                
-            </main>
-        </>
-    )
-};
+	return (
+		<>
+			<main className="md:w-[80%] flex items-center justify-center flex-col gap-5 mx-auto">
+				<div className={`h-200 flex items-center justify-center`}>
+					<AboutBox />
+				</div>
+
+				<AboutSkill />
+			</main>
+		</>
+	);
+}
 
 export default aboutMe;
