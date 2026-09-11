a31c9ba61b0fd4018d1ebad77e8ab88d8a9175e1
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-11 21:11:20 +0330

    Add theme light to Statistic component, flexible statistics

--- Frontend/components/About-me/AboutStatistics/AboutStatistics.tsx
+++ Frontend/components/About-me/AboutStatistics/AboutStatistics.tsx
@@ -8,34 +8,34 @@ function AboutStatistics() {
     const { isDark } = useThemeContext();
     return (
         <>
-            <section className={`grid grid-cols-1`}>
+            <section className={`grid grid-cols-1 md:grid-cols-2 md:gap-4 lg:grid-cols-4 pt-5 lg:gap-10`}>
                 <Statistics 
                     icon={faListCheck}
                     name="تعداد پروژه"
                     content="+3"
-                    className={isDark ? "text-blue-400 hover:border-blue-400" : ""}
-                    classTitle={isDark ? "group-hover:text-blue-400" : ""}
+                    className={isDark ? "text-blue-400 hover:border-blue-400" : "text-blue-400 hover:border-blue-500"}
+                    classTitle={isDark ? "group-hover:text-blue-400" : "group-hover:text-blue-500"}
                 />
                 <Statistics
                     icon={faUsers}
                     name="تعداد اعضای تیم"
                     content="2 نفر"
-                    className={isDark ? "text-pink-400 hover:border-pink-400" : ""}
-                    classTitle={isDark ? "group-hover:text-pink-400" : ""}
+                    className={isDark ? "text-pink-400 hover:border-pink-400" : "text-pink-500 hover:border-pink-500"}
+                    classTitle={isDark ? "group-hover:text-pink-400" : "group-hover:text-pink-500"}
                 />
                 <Statistics
                     icon={faStar}
                     name="رضایت مشتری"
                     content="85%"
-                    className={isDark ? "text-yellow-400 hover:border-yellow-400" : ""}
-                    classTitle={isDark ? "group-hover:text-yellow-400" : ""}
+                    className={isDark ? "text-yellow-400 hover:border-yellow-400" : "text-yellow-500 hover:border-yellow-500"}
+                    classTitle={isDark ? "group-hover:text-yellow-400" : "group-hover:text-yellow-600"}
                 />
                 <Statistics
                     icon={faCalendar}
                     name="تجربه"
                     content="+1 سال"
-                    className={isDark ? "text-green-400 hover:border-green-400" : ""}
-                    classTitle={isDark ? "group-hover:text-green-400" : ""}
+                    className={isDark ? "text-green-400 hover:border-green-400" : "text-green-600 hover:border-green-500"}
+                    classTitle={isDark ? "group-hover:text-green-400" : "group-hover:text-green-600"}
                 />
             </section>
         </>
--- Frontend/components/About-me/AboutStatistics/Statistics.tsx
+++ Frontend/components/About-me/AboutStatistics/Statistics.tsx
@@ -7,13 +7,13 @@ function Statistics({ icon, name, content, className, classIcon, classTitle, cla
     const { isDark } = useThemeContext();
     return (
         <>
-            <div className={`group border-b ${className ? className : ""} ${isDark ? "border-[#252525]" : ""} hover:scale-105 transition-all duration-150 py-2 px-5 mt-5 flex items-center justify-center flex-col gap-2 `}>
+            <div className={`group border-b ${className ? className : ""} ${isDark ? "border-[#252525]" : "border-gray-300"} hover:scale-110 transition-all duration-150 py-2 px-5 mt-5 flex items-center justify-center flex-col gap-2 `}>
                 <FontAwesomeIcon
                     icon={icon}
                     className={`${classIcon ? classIcon : ""} text-2xl`}
                 />
-                <h1 className={` ${classTitle ? classTitle : ""} transition-all  duration-150  text-lg font-bold ${isDark ? "text-gray-300" : ""} `}>{name}</h1>
-                <h2 className={` ${classContent ? classContent : ""} ${isDark ? "text-gray-100" : ""}`}>{content}</h2>
+                <h1 className={` ${classTitle ? classTitle : ""} transition-all  duration-150  text-lg font-bold ${isDark ? "text-gray-300" : "text-gray-700"} `}>{name}</h1>
+                <h2 className={` ${classContent ? classContent : ""} ${isDark ? "text-gray-100" : "text-[#252525]"}`}>{content}</h2>
             </div>
         </>
     )
