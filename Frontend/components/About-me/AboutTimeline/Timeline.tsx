08c00810db7b3f454b19e22540899c3497499d08
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-11 22:27:22 +0330

    Complete design of timeline component

--- Frontend/components/About-me/AboutTimeline/Timeline.tsx
+++ Frontend/components/About-me/AboutTimeline/Timeline.tsx
@@ -6,7 +6,15 @@ function Timeline({ year, title, content, className, classYear, classTitle, clas
     const { isDark } = useThemeContext();
     return (
         <>
-        
+            <div className={` ${className ? className : ""} flex items-center justify-center gap-3 px-2 py-3 rounded-md ${isDark ? "bg-[#252525]" : ""}`}>
+                <div className={`px-2 py-3 pt-4 md:px-4 md:py-5 md:pt-6 flex ${isDark ? "bg-[#151515] rounded-md" : ""} items-center justify-center`}>
+                    <h1 className={`${classYear ? classYear : ""} md:text-xl font-bold ${isDark ? "text-gray-100" : ""}`}>{year}</h1>
+                </div>
+                <div>
+                    <h1 className={`${classTitle ? classTitle : ""} md:text-xl ${isDark ? "text-gray-300" : ""}`}>{title}</h1>
+                    <h2 className={`${classContent ? classContent : ""} hidden md:inline-block md:h-10 text-sm pt-2 ${isDark ? "text-[#9e9e9e]" : ""}`}>{content}</h2>
+                </div>
+            </div>
         </>
     )
 };
