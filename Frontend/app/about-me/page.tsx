80161ec51265f0e268bd360b4d375b46555d2be5
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-11 17:35:30 +0330

    Edit height of AboutBox component

--- Frontend/app/about-me/page.tsx
+++ Frontend/app/about-me/page.tsx
@@ -1,3 +1,4 @@
+
 import AboutBox from "@/components/About-me/AboutBox";
 import AboutSkill from "@/components/About-me/AboutSkill";
 
@@ -5,7 +6,7 @@ function aboutMe() {
 	return (
 		<>
 			<main className="md:w-[80%] flex items-center justify-center flex-col gap-5 mx-auto">
-				<div className={`h-200 flex items-center justify-center`}>
+				<div className={`h-180 flex items-center justify-center `}>
 					<AboutBox />
 				</div>
 
