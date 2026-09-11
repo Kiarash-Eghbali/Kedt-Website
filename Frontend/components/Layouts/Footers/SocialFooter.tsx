37499131f7bf74c556fbc153ab9e4a2a394f0f0a
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-11 15:09:26 +0330

    Add aria-label to social footer links

--- Frontend/components/Layouts/Footers/SocialFooter.tsx
+++ Frontend/components/Layouts/Footers/SocialFooter.tsx
@@ -19,6 +19,7 @@ function SocialFooter() {
 					<Link
 						href={"/"}
 						className={`px-2 py-2 md:px-3 md:py-3 flex items-center justify-center rounded-md group ${isDark ? "bg-[#252525] border border-[#252525]" : "bg-white border border-gray-300"} hover:-translate-y-1 transition-all duration-150`}
+						aria-label="Link is my telegram"
 					>
 						<FontAwesomeIcon
 							icon={faTelegram}
@@ -28,6 +29,7 @@ function SocialFooter() {
 					<Link
 						href={"/"}
 						className={`px-2 py-2 md:px-3 md:py-3 flex items-center justify-center rounded-md group ${isDark ? "bg-[#252525] border border-[#252525]" : "bg-white border border-gray-300"} hover:-translate-y-1 transition-all duration-150`}
+						aria-label="Link is my LinkedIn"
 					>
 						<FontAwesomeIcon
 							icon={faLinkedin}
@@ -37,6 +39,7 @@ function SocialFooter() {
 					<Link
 						href={"/"}
 						className={`px-2 py-2 md:px-3 md:py-3 flex items-center justify-center rounded-md group ${isDark ? "bg-[#252525] border border-[#252525]" : "bg-white border border-gray-300"} hover:-translate-y-1 transition-all duration-150`}
+						aria-label="Link is my Github"
 					>
 						<FontAwesomeIcon
 							icon={faGithubSquare}
