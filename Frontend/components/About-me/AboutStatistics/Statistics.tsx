0356f4de13c174a86bbb79be9fe4a553ab25199a
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-11 20:26:28 +0330

    Create interface for statistics component

--- Frontend/components/About-me/AboutStatistics/Statistics.tsx
+++ Frontend/components/About-me/AboutStatistics/Statistics.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { useThemeContext } from "@/contexts/ThemeContext";
+import StatisticsInterface from "@/types/StatisticsInterface";
 
-function Statistics() {
+function Statistics({ icon, name, content, className, classTitle }: StatisticsInterface) {
     const { isDark } = useThemeContext();
     return (
         <>
-            
+
         </>
     )
 };
---
+++ Frontend/types/StatisticsInterface.tsx
@@ -0,0 +1,11 @@
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+
+interface StatisticsInterface {
+    icon: IconProp;
+    name: string;
+    content: string;
+    className: string;
+    classTitle: string;
+};
+
+export default StatisticsInterface;
