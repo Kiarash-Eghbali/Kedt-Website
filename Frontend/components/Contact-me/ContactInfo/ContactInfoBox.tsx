bfd5a329d4554a51f6aebb08bb0e4961632bfbb1
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-12 16:08:41 +0330

    feat(contact): add contactInfo component

--- Frontend/components/Contact-me/ContactInfo/ContactInfoBox.tsx
+++ Frontend/components/Contact-me/ContactInfo/ContactInfoBox.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useThemeContext } from "@/contexts/ThemeContext";
+import ContactInfo from "./ContactInfo";
 
 
 function ContactInfoBox() {
@@ -8,6 +9,7 @@ function ContactInfoBox() {
         <>  
             <section className={`w-full p-1 md:w-[80%] mt-10 flex items-center justify-center md:items-start md:justify-start flex-col`}>
                 <h1 className={`text-xl text-center ${isDark ? "text-[#888888] font-bold" : "text-gray-600 font-bold"}`}>تماس با من</h1>
+                <ContactInfo />
             </section>
         </>
     )
