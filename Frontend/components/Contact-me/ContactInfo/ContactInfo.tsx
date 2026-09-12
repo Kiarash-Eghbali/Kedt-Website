76ad0e9f07d4f05ea0b3f754d4c67498ed373ab6
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-12 18:20:53 +0330

    style(contact-info): add responsibility

--- Frontend/components/Contact-me/ContactInfo/ContactInfo.tsx
+++ Frontend/components/Contact-me/ContactInfo/ContactInfo.tsx
@@ -1,13 +1,15 @@
 "use client";
 import { useThemeContext } from "@/contexts/ThemeContext";
 import InfoParagraph from "./InfoParagraph";
+import Info from "./Info";
 
 function ContactInfo() {
 	const { isDark } = useThemeContext();
 	return (
 		<>
-			<section className={`grid grid-cols-1`}>
+			<section className={`grid grid-cols-1 items-start justify-start  gap-5`}>
                 <InfoParagraph />
+                <Info />
 			</section>
 		</>
 	);
