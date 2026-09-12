0ea474589ed05e5f80380e11edcfaa906157b03d
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-12 18:24:53 +0330

    feat(contact-info): create info component and add Social component to this

---
+++ Frontend/components/Contact-me/ContactInfo/Info.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { useThemeContext } from "@/contexts/ThemeContext";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import Social from "./Social";
+import { faInstagram, faLinkedin, faTelegram } from "@fortawesome/free-brands-svg-icons";
+
+
+function Info() {
+	const { isDark } = useThemeContext();
+	return (
+		<>
+            <div className={`w-[90%] mx-auto md:w-full grid grid-cols-1 md:grid-cols-2 lg:flex items-center justify-center gap-5`}>
+                <Social 
+                    icon={faEnvelope}
+                    content="king.game.live89@gmail.com"
+                    className={isDark ? "text-red-400" : ""}
+                />
+                <Social
+                    icon={faTelegram}
+                    content="KiarashEgh@"
+                    className={isDark ? "text-blue-400" : ""}
+                />
+                <Social
+                    icon={faLinkedin}
+                    content="KiarashEghbali@"
+                    className={isDark ? "text-blue-500" : ""}
+                />
+                <Social
+                    icon={faInstagram}
+                    content="KiarashJson@"
+                    className={isDark ? "text-pink-600" : ""}
+                />
+            </div>
+		</>
+	);
+}
+
+export default Info;
