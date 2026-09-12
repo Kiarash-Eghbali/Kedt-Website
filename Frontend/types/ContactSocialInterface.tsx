46d459735a1bcc1ec2271163761079e459e79db0
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-12 18:26:35 +0330

    feat(social-interface): create ContactSocialInterface

---
+++ Frontend/types/ContactSocialInterface.tsx
@@ -0,0 +1,9 @@
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+
+interface ContactSocialInterface {
+    icon: IconProp;
+    content: string;
+    className?: string;
+}
+
+export default ContactSocialInterface;
