42ddfecfdf7374b7f307efa12a8d97eda78a6e85
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-13 00:35:26 +0330

    feat(faq-item): add interface to argument

--- Frontend/components/Contact-me/ContactFAQ/FAQItem.tsx
+++ Frontend/components/Contact-me/ContactFAQ/FAQItem.tsx
@@ -1,13 +1,14 @@
 "use client";
 import { useThemeContext } from "@/contexts/ThemeContext"; 
+import FAQInterface from "@/types/FAQInterface";
 import { useState } from "react";
 
-function FAQItem() {
+function FAQItem({ title, content, className }: FAQInterface) {
     const { isDark } = useThemeContext();
     const [ isOpen, setIsOpen ] = useState<boolean>(false)
     return (
         <>
-        
+
         </>
     )
 };
