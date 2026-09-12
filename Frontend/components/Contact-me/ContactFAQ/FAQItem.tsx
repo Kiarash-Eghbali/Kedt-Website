d817ebaa32194b4650b06ae6c62d3cbd5314a6a4
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-13 01:33:24 +0330

    fix(faq-item): adjust height and change onClick event to parent div

--- Frontend/components/Contact-me/ContactFAQ/FAQItem.tsx
+++ Frontend/components/Contact-me/ContactFAQ/FAQItem.tsx
@@ -1,41 +1,40 @@
 "use client";
-import { useThemeContext } from "@/contexts/ThemeContext"; 
+import { useThemeContext } from "@/contexts/ThemeContext";
 import FAQInterface from "@/types/FAQInterface";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 
 function FAQItem({ title, content, className }: FAQInterface) {
-    const { isDark } = useThemeContext();
-    const [ isOpen, setIsOpen ] = useState<boolean>(false)
-    return (
-        <>
-            <div className={`w-[95%] mx-auto flex md:w-[80%] items-center px-1 py-3 rounded-md flex-col ${isDark ? "bg-[#252525]" : "bg-[#eeeeee] border border-[#d1d1d1]"}`}>
-                <div className={`${className ? className : ""} self-start w-full flex items-center gap-3 px-1`}>
-                    <div className={`px-2 py-3 pt-4 md:px-4 md:py-5 md:pt-6 flex ${isDark ? "bg-[#151515] rounded-md" : "bg-white rounded-md"} items-center justify-center`}>
-                    </div>
-                    <div className={`grid grid-cols-4 md:grid-cols-1 w-full`}>
-                        <h1 className={`col-span-3 text-xs md:text-xl ${isDark ? "text-gray-300" : "text-[#111111]"}`}>{title}</h1>
-                        <span className="text-left pl-2 md:hidden">
-                            <FontAwesomeIcon
-                                icon={faArrowRight}
-                                onClick={() => setIsOpen(! isOpen)}
-                                className={`text-lg font-bold cursor-pointer ${isOpen ? "rotate-90" : ""} transition-all duration-150`}
-                            />
-                        </span>
-                    </div>
-                </div>
-                
-                <div className="w-[90%] md:hidden overflow-hidden">
-                    <h2 
-                        className={`w-full px-1 mx-auto text-xs mt-2 transition-all duration-150 ease-in-out ${isDark ? "text-[#9e9e9e]" : ""} ${isOpen ? "max-h-96 opacity-100" : "max-h-0 opacity-0 mt-0"}`}
-                    >
-                        {content}
-                    </h2>
-                </div>
-            </div>
-        </>
-    )
-};
+	const { isDark } = useThemeContext();
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+
+	return (
+		<>
+			<div
+				onClick={() => setIsOpen(!isOpen)}
+				className={`w-[95%]  mx-auto flex md:w-[70%] items-start px-1 py-3 rounded-md flex-col  ${isDark ? "bg-[#252525] " : "bg-[#eeeeee] border border-[#d1d1d1]"}`}
+			>
+				<div className={`${className ? className : ""} pt-3 self-start w-full flex items-center gap-3 px-1`}>
+					<div className={`flex items-center justify-between  w-full`}>
+						<h1 className={`text-xs md:text-lg select-none ${isDark ? "text-gray-300" : "text-[#252525]"}`}>{title}</h1>
+						<span className="text-left pl-2 ">
+							<FontAwesomeIcon
+								icon={faArrowRight}
+								className={`text-lg font-bold cursor-pointer   ${isOpen ? `rotate-90  ${isDark ? "text-blue-500" : "text-blue-600"}` : isDark ? "" : "text-gray-600"}  transition-all duration-150`}
+							/>
+						</span>
+					</div>
+				</div>
+
+				<div className="w-[90%]  overflow-hidden">
+					<h2 className={`w-full md:text-base select-none px-1 mx-auto text-xs mt-2 transition-all duration-150 ease-in-out ${isDark ? "text-[#9e9e9e]" : "text-gray-600"} ${isOpen ? "max-h-96 opacity-100" : "max-h-0 opacity-0 mt-0"}`}>
+						{content}
+					</h2>
+				</div>
+			</div>
+		</>
+	);
+}
 
 export default FAQItem;
