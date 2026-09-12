8dbef89fe7d64735140a5701b9522ac671575e71
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-12 15:25:08 +0330

    feat(contact-info): create ContactInfoBox component

---
+++ Frontend/components/Contact-me/ContactInfo/ContactInfoBox.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { useThemeContext } from "@/contexts/ThemeContext";
+
+
+function ContactInfoBox() {
+    const { isDark } = useThemeContext();
+    return (
+        <>  
+            <section className={`w-full p-1 md:w-[80%] mt-10 flex items-center justify-center md:items-start md:justify-start flex-col`}>
+                <h1 className={`text-xl text-center ${isDark ? "text-[#888888] font-bold" : "text-gray-600 font-bold"}`}>تماس با من</h1>
+            </section>
+        </>
+    )
+};
+
+export default ContactInfoBox;
