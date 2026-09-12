4e4108dd11ae6fb813f93a8d92b9a4baba5ec76c
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-12 15:32:40 +0330

    style(contact): adjust margin top

--- Frontend/app/contact-me/page.tsx
+++ Frontend/app/contact-me/page.tsx
@@ -3,7 +3,7 @@ import ContactInfoBox from "@/components/Contact-me/ContactInfo/ContactInfoBox";
 function contactMe() {
     return (
         <>
-            <main className="md:w-[80%] flex items-center justify-center flex-col gap-30 mx-auto">
+            <main className="md:w-[80%] mt-30 flex items-center justify-center flex-col gap-30 mx-auto">
                 <ContactInfoBox />
             </main>
         </>
