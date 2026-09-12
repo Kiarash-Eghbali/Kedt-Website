f217d51bd1ba8db18281f932bee595ffdc3c2200
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-13 00:28:34 +0330

    feat(faq-box): create FAQBox component

---
+++ Frontend/components/Contact-me/ContactFAQ/FAQBox.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { useThemeContext } from "@/contexts/ThemeContext";
+
+
+function FAQBox() {
+    const { isDark } = useThemeContext();
+    return (
+        <>
+            
+        </>
+    )
+};
+
+export default FAQBox;
