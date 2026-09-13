40784a1ab07c286a34a6eecb06b7e498bd331cdf
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-13 12:27:48 +0330

    refactor(view-middleware): refactor viewMiddleware

--- Backend/src/middlewares/viewMiddleware.ts
+++ Backend/src/middlewares/viewMiddleware.ts
@@ -1,10 +1,14 @@
 import { type Request, type Response, type NextFunction } from "express";
 
-function viewMiddleware(req: Request, res: Response, next: NextFunction) {
-    const page = req.params.page;
-    const cookieName = `viewed_${page}`;
-    req.hasViewed = !!req.cookies[cookieName];
-    next();
-}
+const viewMiddleware = function (req: Request, res: Response, next: NextFunction) {
+	const cookie = req.cookies;
+	if (!cookie.viewed) {
+		req.hasViewed = false;
+	} else {
+		req.hasViewed = true;
+	}
+
+	next();
+};
 
 export default viewMiddleware;
