b72474aa8d5d0a5cd513807d8432c7187b97a3b2
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-13 23:40:02 +0330

    fix(social-footer): add links to href

--- Frontend/components/Layouts/Footers/SocialFooter.tsx
+++ Frontend/components/Layouts/Footers/SocialFooter.tsx
@@ -17,7 +17,7 @@ function SocialFooter() {
 				<h1 className={`text-sm pt-3 md:text-lg font-bold ${isDark ? "text-gray-300" : "text-gray-700"}`}>شبکه های اجتماعی</h1>
 				<div className={`flex items-center pt-5 gap-5 justify-start`}>
 					<Link
-						href={"/"}
+						href={"https://t.me/KiarashEgh"}
 						className={`px-2 py-2 md:px-3 md:py-3 flex items-center justify-center rounded-md group ${isDark ? "bg-[#252525] border border-[#252525]" : "bg-white border border-gray-300"} hover:-translate-y-1 transition-all duration-150`}
 						aria-label="Link is my telegram"
 					>
@@ -27,7 +27,7 @@ function SocialFooter() {
 						/>
 					</Link>
 					<Link
-						href={"/"}
+						href={"https://www.linkedin.com/in/kiarash-eghbali-07968442b/"}
 						className={`px-2 py-2 md:px-3 md:py-3 flex items-center justify-center rounded-md group ${isDark ? "bg-[#252525] border border-[#252525]" : "bg-white border border-gray-300"} hover:-translate-y-1 transition-all duration-150`}
 						aria-label="Link is my LinkedIn"
 					>
@@ -37,7 +37,7 @@ function SocialFooter() {
 						/>
 					</Link>
 					<Link
-						href={"/"}
+						href={"https://github.com/Kiarash-Eghbali"}
 						className={`px-2 py-2 md:px-3 md:py-3 flex items-center justify-center rounded-md group ${isDark ? "bg-[#252525] border border-[#252525]" : "bg-white border border-gray-300"} hover:-translate-y-1 transition-all duration-150`}
 						aria-label="Link is my Github"
 					>
