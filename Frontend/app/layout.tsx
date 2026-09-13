8df12de453f6e5f2e148bd530581601484c6ab01
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-13 19:59:09 +0330

    feat(layout): add view and reaction context provider

--- Frontend/app/layout.tsx
+++ Frontend/app/layout.tsx
@@ -4,21 +4,17 @@ import "./globals.css";
 import Header from "@/components/Layouts/Header";
 import Footer from "@/components/Layouts/Footer";
 import { ThemeProvider } from "@/contexts/ThemeContext";
+import { ViewProvider } from "@/contexts/ViewContext";
+import { ToastContainer } from "react-toastify";
+import { ReactionProvider } from "@/contexts/ReactionContext";
 
-const vazirMatn = Vazirmatn({ 
-	subsets: ["arabic"],
-	display: "swap",
-	preload: true,
-	adjustFontFallback: true,
-});
-const poppins = Poppins({ 
-	subsets: ["latin"],
-	display: "swap",
-	variable: "--font-poppins",
-	weight: ["500"] 
-});
+const vazirMatn = Vazirmatn({ subsets: ["arabic"], display: "swap", preload: true, adjustFontFallback: true });
+const poppins = Poppins({ subsets: ["latin"], display: "swap", variable: "--font-poppins", weight: ["500"] });
 
-export const metadata: Metadata = { title: "KEDT | تیم توسعه دهنده کیارش اقبالی", description: "KEDT website created with NextJs , Typescript , Tailwindcss , ExpressJs , NodeJs" };
+export const metadata: Metadata = {
+	title: "KEDT | تیم توسعه دهنده کیارش اقبالی",
+	description: "KEDT website created with NextJs , Typescript , Tailwindcss , ExpressJs , NodeJs",
+};
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
 	return (
@@ -28,9 +24,24 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
 			className={`${vazirMatn.className} ${poppins.variable} h-full antialiased`}
 		>
 			<ThemeProvider>
-				<Header />
-				{children}
-				<Footer />
+				<ToastContainer
+					position="top-center"
+					autoClose={3000}
+					hideProgressBar={false}
+					newestOnTop
+					closeOnClick
+					rtl={true}
+					pauseOnFocusLoss
+					draggable
+					pauseOnHover
+				/>
+				<ViewProvider>
+					<ReactionProvider>
+						<Header />
+						{children}
+						<Footer />
+					</ReactionProvider>
+				</ViewProvider>
 			</ThemeProvider>
 		</html>
 	);
