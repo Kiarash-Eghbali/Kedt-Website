d9ba379346e1057266212db90e054cfe55ae5d53
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-13 20:00:18 +0330

    feat(like-count): add view and reaction context values

--- Frontend/components/Home/LikeSection/LikeCountBox.tsx
+++ Frontend/components/Home/LikeSection/LikeCountBox.tsx
@@ -1,5 +1,7 @@
 "use client";
+import { useReactionContext } from "@/contexts/ReactionContext";
 import { useThemeContext } from "@/contexts/ThemeContext";
+import { useViewContext } from "@/contexts/ViewContext";
 import { faThumbsDown, faThumbsUp, faEye } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -8,7 +10,8 @@ const poppins = Poppins({ subsets: ["latin"], variable: "--font-poppins", weight
 
 function LikeCountBox() {
 	const { isDark } = useThemeContext();
-
+	const views = useViewContext();
+	const { likes, dislikes } = useReactionContext();
 
 	return (
 		<>
@@ -19,7 +22,7 @@ function LikeCountBox() {
 						className={` ${isDark ? "text-xl text-green-500" : "text-xl text-green-500"} `}
 					/>
 				</div>
-				<h1 className={`text-xl ${poppins.className} ${isDark ? "" : "text-gray-600"}`}>0</h1>
+				<h1 className={`text-xl ${poppins.className} ${isDark ? "" : "text-gray-600"}`}>{likes}</h1>
 			</div>
 			<div className={` flex items-center justify-center gap-2 px-3 py-2  ${isDark ? "bg-[#252525] rounded-xl" : "border border-[#d1d1d1] bg-white rounded-xl"}`}>
 				<div className={`flex items-center justify-center gap-1`}>
@@ -28,7 +31,7 @@ function LikeCountBox() {
 						className={` ${isDark ? "text-xl text-red-500" : "text-xl text-red-500"} `}
 					/>
 				</div>
-				<h1 className={`text-xl ${poppins.className} ${isDark ? "" : "text-gray-600"}`}>0</h1>
+				<h1 className={`text-xl ${poppins.className} ${isDark ? "" : "text-gray-600"}`}>{dislikes}</h1>
 			</div>
 			<div className={` flex items-center justify-center gap-2 px-3 py-2  ${isDark ? "bg-[#252525] rounded-xl" : "border border-[#d1d1d1] bg-white rounded-xl"}`}>
 				<div className={`flex items-center justify-center gap-1`}>
@@ -37,7 +40,7 @@ function LikeCountBox() {
 						className={` ${isDark ? "text-xl text-blue-500" : "text-blue-500 text-xl"} `}
 					/>
 				</div>
-				<h1 className={`text-xl ${poppins.className} ${isDark ? "" : "text-gray-600"}`}>0</h1>
+				<h1 className={`text-xl ${poppins.className} ${isDark ? "" : "text-gray-600"}`}>{views}</h1>
 			</div>
 		</>
 	);
