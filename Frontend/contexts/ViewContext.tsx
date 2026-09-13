c8161d977fa1c49e6745030dc2b101356eccf603
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-13 19:57:18 +0330

    feat(view-context): create view context

---
+++ Frontend/contexts/ViewContext.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import { useThemeContext } from "./ThemeContext";
+
+const ViewContext = createContext<number | null>(null);
+
+export function ViewProvider({ children }: { children: ReactNode }) {
+	const [views, setViews] = useState<number>(0);
+    const { isDark } = useThemeContext();
+
+	useEffect(() => {
+		async function getViews() {
+			let res = await fetch("http://localhost:5000/reaction", { credentials: "include" });
+
+			if (res.ok) {
+				const data = await res.json();
+				setViews(data.values.views)
+			}
+		}
+		getViews();
+	}, []);
+
+	return (
+		<>
+			<ViewContext.Provider value={views}>{children}</ViewContext.Provider>
+		</>
+	);
+}
+
+export function useViewContext() {
+	const context = useContext(ViewContext);
+	if (context === null) {
+		throw new Error("ViewContext must be used within ViewProvider");
+	}
+	return context;
+}
