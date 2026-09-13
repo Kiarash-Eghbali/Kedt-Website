7b9d51a5ad05e354616a4243a8cddff554d7944f
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-13 11:04:16 +0330

    feat(server): add database and express json

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,15 +1,18 @@
 import express, { type Express } from "express";
 import cookieParser from "cookie-parser";
-import cors from "cors";
-import "dotenv/config";
-require("dotenv/config").config();
+import dotenv from "dotenv";
+import connectDB from "./configs/db";
+
+dotenv.config();
+
+connectDB();
 
 const app: Express = express();
+app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
 
 const PORT: number | string = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log("KEDT Server has been runned");
+	console.log("KEDT Server is running");
 });
